441c89f01e475d57e673605e73eeac5e0ec388f6
Cyril Van Leer <vanleercyril@gmail.com>
2026-01-11 16:02:48 +0100

    keep client in the main js file

--- src/script.js
+++ src/script.js
@@ -1,5 +1,7 @@
 // Supabase client
-import { supabaseClient } from "./ui/supabase.js";
+const SUPABASE_URL = "https://fzgljqihruhafuqxvduy.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6Z2xqcWlocnVoYWZ1cXh2ZHV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY5MzkyNjMsImV4cCI6MjA4MjUxNTI2M30.Wz0f7Ss7rosusVAumaa1e1LenvNc4d5a6DGVfYQlTm0";
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 /* ---------- Tabs ---------- */
 import { openTab } from "./ui/tabs.js";
