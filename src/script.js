a6f3e43c45bfafd4952a91d75cafcc699c6e5d1f
Cyril Van Leer <vanleercyril@gmail.com>
2026-01-11 15:53:41 +0100

    add ui subfolder

--- src/script.js
+++ src/script.js
@@ -1,10 +1,8 @@
 // Supabase client
-const SUPABASE_URL = "https://fzgljqihruhafuqxvduy.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6Z2xqcWlocnVoYWZ1cXh2ZHV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY5MzkyNjMsImV4cCI6MjA4MjUxNTI2M30.Wz0f7Ss7rosusVAumaa1e1LenvNc4d5a6DGVfYQlTm0";
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+import { supabaseClient } from "./ui/supabase.js";
 
 /* ---------- Tabs ---------- */
-import { openTab } from "./utils/tabs.js";
+import { openTab } from "./ui/tabs.js";
 
 
 import { getPressure } from "./utils/pressure.js";
---
+++ src/ui/supabase.js
@@ -0,0 +1,7 @@
+export const SUPABASE_URL = "https://fzgljqihruhafuqxvduy.supabase.co";
+export const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6Z2xqcWlocnVoYWZ1cXh2ZHV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY5MzkyNjMsImV4cCI6MjA4MjUxNTI2M30.Wz0f7Ss7rosusVAumaa1e1LenvNc4d5a6DGVfYQlTm0";
+
+export const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
